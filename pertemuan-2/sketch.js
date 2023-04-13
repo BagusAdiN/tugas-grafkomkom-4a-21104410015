7096c3001f08411ad574989cc4aabf971a2685ef
BagusAdiN <bagusadiblitar@gmail.com>
2023-04-13 15:05:46 +0700

    code_21104410015_RamadhanBAgus

---
+++ pertemuan-2/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(640, 480);
+}
+//Ramadhan Bagus Adi Nugroho
+function draw() {
+  background(47, 79, 79);
+
+  fill(255, 0, 0);
+  stroke(0);
+  strokeWeight(2);
+  quad(100, 100, 200, 100, 250, 200, 150, 200);
+}
